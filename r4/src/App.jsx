786e3c3841f28a45967f19793bb56199c747c8d9
neigiamasJonas <p.ezerskis@gmail.com>
2022-06-15 15:38:08 +0300

    sort with reducer

--- r4/src/App.jsx
+++ r4/src/App.jsx
@@ -1,7 +1,8 @@
 
 import axios from 'axios';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useReducer } from 'react';
 import './App.scss';
+import sortReducer from './Reducer/sort';
 
 function App() {
 
@@ -12,24 +13,78 @@ function App() {
     .then(res => {
       setScoo(res.data)
     })
+  }, [scoo])
+
+  const [sort, setSort] = useState('ID')
+  const [scooters, dispachScooters] = useReducer(sortReducer, scoo)
+
+
+  useEffect(() => {
+    axios.get('http://localhost:3003/scooters')
+    .then(res => {
+
+      dispachScooters({type: sort, payload: res.data})
+    })
   })
 
 
   return (
     <div>
       <h1>FAULT-DB</h1>
-      <div>
+      <select value={sort} onChange={e => setSort(e.target.value)}>
+        <option value="ID">ID</option>
+        <option value="KM">KM</option>
+        <option value="Date">Date</option>
+      </select>
+      <div style={{display: 'flex', flexDirection: 'row'}}>
+        <div>
+          {
+            sort === 'ID' ?         
+                [...scoo].sort((a, b) => (a.id - b.id)).map(a => <ul key={a.id}><b>ID: {a.id}</b>
+                <li>Registration Code: <b>{a.reg_code}</b></li>
+                <li>State: <b>{a.state}</b></li>
+                <li>Km: <b>{a.km}</b> km</li>
+                <li>Last Time Used: <b>{a.date.slice(0, 10)}</b></li>
+                <li>State: <b>{a.busy === 1 ? 'Busy' : 'Free'}</b></li>
+              </ul>)
+              : null
+          }
+                  {
+            sort === 'KM' ?         
+                [...scoo].sort((a, b) => (a.km - b.km)).map(a => <ul key={a.id}><b>ID: {a.id}</b>
+                <li>Registration Code: <b>{a.reg_code}</b></li>
+                <li>State: <b>{a.state}</b></li>
+                <li>Km: <b>{a.km}</b> km</li>
+                <li>Last Time Used: <b>{a.date.slice(0, 10)}</b></li>
+                <li>State: <b>{a.busy === 1 ? 'Busy' : 'Free'}</b></li>
+              </ul>)
+              : null
+          }
+          {
+            sort === 'Date' ?         
+                [...scoo].sort((a, b) => a.date.slice(0, 10).localeCompare(b.date.slice(0, 10))).map(a => <ul key={a.id}><b>ID: {a.id}</b>
+                <li>Registration Code: <b>{a.reg_code}</b></li>
+                <li>State: <b>{a.state}</b></li>
+                <li>Km: <b>{a.km}</b> km</li>
+                <li>Last Time Used: <b>{a.date.slice(0, 10)}</b></li>
+                <li>State: <b>{a.busy === 1 ? 'Busy' : 'Free'}</b></li>
+              </ul>)
+              : null
+          }
+        </div>
+        <div>
         {
-          scoo.map(a => <ul key={a.id}><b>ID: {a.id}</b>
-            <li>Registration Code: <b>{a.reg_code}</b></li>
-            <li>State: <b>{a.state}</b></li>
-            <li>Km: <b>{a.km}</b> km</li>
-            <li>Last Time Used: <b>{a.date.slice(0, 10)}</b></li>
-            <li>State: <b>{a.busy === 1 ? 'Busy' : 'Free'}</b></li>
-          </ul>)
-        }
+                scooters.map(a => (
+                <ul key={a.id}><b>ID: {a.id}</b>
+                  <li>Registration Code: <b>{a.reg_code}</b></li>
+                  <li>State: <b>{a.state}</b></li>
+                  <li>Km: <b>{a.km}</b> km</li>
+                  <li>Last Time Used: <b>{a.date.slice(0, 10)}</b></li>
+                  <li>State: <b>{a.busy === 1 ? 'Busy' : 'Free'}</b></li>
+                </ul>))
+          }
+        </div>
       </div>
-
     </div>
   );
 }
---
+++ r4/src/Reducer/sort.js
@@ -0,0 +1,30 @@
+function sortReducer(state, action) {
+
+    let newState;
+
+    switch(action.type) {
+
+        case "ID":
+            newState = action.payload.sort((a, b) => (a.id - b.id))
+
+            break;
+
+        case "KM":
+            newState = action.payload.sort((a, b) => (a.km - b.km))
+    
+            break;
+
+        case "Date":
+            newState = action.payload.sort((a, b) => a.date.localeCompare(b.date))
+        
+            break;
+
+        default:
+            newState = [...state]
+    }
+
+
+    return newState
+}
+
+export default sortReducer;

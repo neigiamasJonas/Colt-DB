2236d10af2ffa085f207edb6d5da6caafdddd517
neigiamasJonas <p.ezerskis@gmail.com>
2022-06-15 12:33:33 +0300

    faultDB - get demo data

--- r4/src/App.jsx
+++ r4/src/App.jsx
@@ -1,10 +1,34 @@
 
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 import './App.scss';
 
 function App() {
+
+  const [scoo, setScoo] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:3003/scooters')
+    .then(res => {
+      setScoo(res.data)
+    })
+  })
+
+
   return (
     <div>
       <h1>FAULT-DB</h1>
+      <div>
+        {
+          scoo.map(a => <ul key={a.id}><b>ID: {a.id}</b>
+            <li>Registration Code: <b>{a.reg_code}</b></li>
+            <li>State: <b>{a.state}</b></li>
+            <li>Km: <b>{a.km}</b> km</li>
+            <li>Last Time Used: <b>{a.date.slice(0, 10)}</b></li>
+            <li>State: <b>{a.busy === 1 ? 'Busy' : 'Free'}</b></li>
+          </ul>)
+        }
+      </div>
 
     </div>
   );
